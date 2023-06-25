e062852ec36f10bf41e40ee984e84665848b4b48
ChokoJoestar <mohamedboulidam680@gmail.com>
2023-06-25 16:21:40 +0200

    Add a new lang

--- src/main/java/fr/chokojoestar/capymod/data/CapyEnglishLangProvider.java
+++ src/main/java/fr/chokojoestar/capymod/data/CapyEnglishLangProvider.java
@@ -1,5 +1,6 @@
 package fr.chokojoestar.capymod.data;
 
+import fr.chokojoestar.capymod.entity.CapyEntities;
 import fr.chokojoestar.capymod.item.CapyItems;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
 import net.fabricmc.fabric.api.datagen.v1.provider.FabricLanguageProvider;
@@ -14,6 +15,7 @@ public class CapyEnglishLangProvider extends FabricLanguageProvider {
    public void generateTranslations(TranslationBuilder translationBuilder) {
       translationBuilder.add(CapyItems.STAFF, "Staff");
       translationBuilder.add(CapyItems.CAPYBARA_SPAWN_EGG, "Capybara Spawn Egg");
+      translationBuilder.add(CapyEntities.CAPYBARA, "Capybara");
    }
 
 }
